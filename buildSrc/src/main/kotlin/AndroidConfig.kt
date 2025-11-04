b32987b4306bb8ddbd4fbd9a052c2ea334e64b14
Samfun <38332931+Samfun75@users.noreply.github.com>
2025-11-04 10:10:35 +0300

    fix: downgrade some dependencies for inspector compatibility

--- buildSrc/src/main/kotlin/AndroidConfig.kt
+++ buildSrc/src/main/kotlin/AndroidConfig.kt
@@ -1,7 +1,7 @@
 object AndroidConfig {
-    const val compileSdk = 36
+    const val compileSdk = 34
     const val minSdk = 26
-    const val targetSdk = 36
+    const val targetSdk = 34
     const val namespace = "eu.kanade.tachiyomi.animeextension"
     const val coreNamespace = "eu.kanade.tachiyomi.lib.core"
     const val multisrcNamespace = "eu.kanade.tachiyomi.lib.themesources"
